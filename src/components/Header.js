391bfa0dddd28db26f687f1985e48c81f120a8fd
github username <aslongman22@gmail.com>
2020-06-10 11:13:59 -0400

    header start

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <div>
+            <h1>Hi, I'm Ben!</h1>
+            <h2>I make animations.</h2>
+        </div>
+    )
+}
